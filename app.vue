415a3f4d58f776527a2b15a1676f48192d356c42
ynakayu <9111754+ynakayu@users.noreply.github.com>
2023-04-04 11:20:47 +0900

    09-04-action

--- app.vue
+++ app.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <p>{{ counter }}</p>
+    <button @click="counterStore.countUp">+</button>
+    <button @click="counterStore.countDown">-</button>
+    <button @click="counterStore.Reset">Reset</button>
   </div>
 </template>
 <script setup>
-  const counter = useCounterState()
+  const counterStore = useCounterState()
+  const { counter } = counterStore
 </script>
